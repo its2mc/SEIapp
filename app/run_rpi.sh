88ec770c3051161d4e1723368f6ca3b3f04dfbac
its2mc <its2uraps@msn.com>
2015-01-12 19:53:01 +0300

    Fixing issues
    Realised I forgot to put separate run files for the beagle and the rpi

---
+++ app/run_rpi.sh
@@ -0,0 +1,9 @@
+ #SEI app runtime
+ cd node 
+ echo "Running node server."
+ sudo node commServer.js > output_node.txt &
+ cd ../python
+ echo "Running python server."
+ sudo python main_rpi.py > output_python.txt &
+ cd ..
+ echo "SEI App running."
