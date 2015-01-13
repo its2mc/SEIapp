d311d1e6901534e9e8bec199331943f04df81644
its2mc <its2uraps@msn.com>
2015-01-13 21:57:42 +0300

    Redid more edits to the bash files to make them UNIX worthy

--- app/run_beagle.sh
+++ app/run_beagle.sh
@@ -1,9 +1,9 @@
- #SEI app runtime
- cd node 
- echo "Running node server."
- sudo node commServer.js > output_node.txt &
- cd ../python
- echo "Running python server."
- sudo python main_beagle.py > output_python.txt &
- cd ..
- echo "SEI App running."
+#SEI app runtime
+cd node
+echo "Running node server."
+sudo node commServer.js > output_node.txt &
+cd ../python
+echo "Running python server."
+sudo python main_beagle.py > output_python.txt &
+cd ..
+echo "SEI app running"
--- app/run_rpi.sh
+++ app/run_rpi.sh
@@ -1,9 +1,9 @@
- #SEI app runtime
- cd node 
- echo "Running node server."
- sudo node commServer.js > output_node.txt &
- cd ../python
- echo "Running python server."
- sudo python main_rpi.py > output_python.txt &
- cd ..
- echo "SEI App running."
+#SEI app runtime
+cd node
+echo "Running node server.s"
+sudo node commServer.js > output_node.txt &
+cd ../python
+echo "Running python server"
+sudo python main_rpi.py > output_python.txt &
+cd ..
+echo "SEI App running."
