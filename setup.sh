f9dfe10a5a83abf90bb67b8092e23fe512b4364c
its2mc <its2uraps@msn.com>
2015-01-13 21:52:59 +0300

    Reedited the file to make UNIX compatible...
    Both the setup file and the run_rpi.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 #The installation must be installed from root with root previledges--
 #The application will be run by opening the SEIapp folder and running the run.sh with sudo bash run.sh--
 
-echo "++Sei Application installation (Will take approximately 2-3 hours to complete--grab a coffee..:) )++"
+echo "++SEI Application installation (Will take approximately 2-3 hours to complete--grab a coffee..:) )++"
 echo
 
 echo "--updating linux env...--"
@@ -14,15 +14,15 @@ sudo apt-get upgrade
 echo
 
 echo "--installing packages needed for compiling...--"
-sudo apt-get install –y g    make
-sudo apt-get install –y g    autoconf
-sudo apt-get install –y g    automake
-sudo apt-get install –y g    libtool
+sudo apt-get install -y g	make
+sudo apt-get install -y g	autoconf
+sudo apt-get install -y g	automake
+sudo apt-get install -y g	libtool
 
 echo
 echo "--installing mysql--"
-echo "--You will be asked if you want to continue with the installation, enter y and press enter--"
-echo "--A screen will appear asking you to set 'New password for the MySQL 'root' user', enter a password, then confirm it. (Do not forget it)--"
+echo "--You will be aksed if you want to continue with the installation, enter y and press enter--"
+echo "--A screen will appear asking you to set 'New password for the MySql 'root' user', enter a password, then confirm it. (Do not forget it)--"
 sudo apt-get install mysql-server mysql-client php5-mysql
 
 echo
@@ -30,22 +30,22 @@ echo "--installing nodejs v0.6--"
 sudo apt-get install nodejs
 
 echo
-echo "--installing nodejs v0.10-"
-wget http://nodejs.org/dist/v0.10.2/node-v0.10.2.tar.gz
+echo "--installing nodejs v0.10--""
+wget http://nodejs.org/dist.v0.10.2/node-v0.10.2.tar.gz
 tar -xzf node-v0.10.2.tar.gz
 cd node-v0.10.2
 ./configure
 make
-sudo make install-
+sudo make install
 cd ..
 
 echo
 echo "--installing zmq--"
 wget http://Download.zeromq.org/zeromq-4.0.3.tar.gz
 tar zxvf zeromq-4.0.3.tar.gz
-cd zeromq-4.0.3 
-./configure 
-make 
+cd zeromq-4.0.3
+./configure
+make
 make install ldconfig
 
 echo
@@ -59,7 +59,7 @@ echo "--If the sql script is successful you will see seidb in the database list 
 sudo mysql -h localhost -u root -p < sqlSetup.sql
 
 echo
-echo "--setting up nodejs environment--"
+echo "--setting up nodejs envirnoment--"
 cd node
 
 echo
@@ -71,7 +71,7 @@ echo "2--installing nodejs express module--"
 sudo npm install express
 
 echo
-echo "3--installing nodejs ws module--"
+echo "3--installing nodejs ws module"
 sudo npm install ws
 
 echo
@@ -105,5 +105,4 @@ cd ../..
 
 echo
 echo "++SEI installation complete++"
-
 
